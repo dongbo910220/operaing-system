a0d4eb125132296a0fd1d6bbe21ebe3cfe337b70
dongbo910220 <1275604947@qq.com>
2020-06-03 21:05:11 +0800

    文件检索
    文件检索功能增加

---
+++ 文件检索/fs.c
@@ -0,0 +1,289 @@
+#include "fs.h"
+#include "super_block.h"
+#include "inode.h"
+#include "dir.h"
+#include "stdint.h"
+#include "stdio-kernel.h"
+#include "list.h"
+#include "string.h"
+#include "ide.h"
+#include "global.h"
+#include "debug.h"
+#include "memory.h"
+
+struct partition* cur_part;
+
+static bool mount_partition(struct list_elem* pelem, int arg) {
+    char* part_name = (char*)arg;
+    struct partition* part = elem2entry(struct partition, part_tag, pelem);
+    if (!strcmp(part->name, part_name)) {
+      cur_part = part;
+      struct disk* hd = cur_part->my_disk;
+      struct super_block* sb_buf = (struct super_block*)sys_malloc(SECTOR_SIZE);
+
+      cur_part->sb = (struct super_block*)sys_malloc(sizeof(struct super_block));
+      if (cur_part->sb == NULL) {
+	 PANIC("alloc memory failed!");
+      }
+
+      /* 读入超级块 */
+      memset(sb_buf, 0, SECTOR_SIZE);
+      ide_read(hd, cur_part->start_lba + 1, sb_buf, 1);
+
+      memcpy(cur_part->sb, sb_buf, sizeof(struct super_block));
+
+      cur_part->block_bitmap.bits = (uint8_t*)sys_malloc(sb_buf->block_bitmap_sects * SECTOR_SIZE);
+      if (cur_part->block_bitmap.bits == NULL) {
+	 PANIC("alloc memory failed!");
+      }
+      cur_part->block_bitmap.btmp_bytes_len = sb_buf->block_bitmap_sects * SECTOR_SIZE;
+      ide_read(hd, sb_buf->block_bitmap_lba, cur_part->block_bitmap.bits, sb_buf->block_bitmap_sects);
+
+
+      cur_part->inode_bitmap.bits = (uint8_t*)sys_malloc(sb_buf->inode_bitmap_sects * SECTOR_SIZE);
+      if (cur_part->inode_bitmap.bits == NULL) {
+	 PANIC("alloc memory failed!");
+    }
+    cur_part->inode_bitmap.btmp_bytes_len = sb_buf->inode_bitmap_sects * SECTOR_SIZE;
+    /* 从硬盘上读入inode位图到分区的inode_bitmap.bits */
+    ide_read(hd, sb_buf->inode_bitmap_lba, cur_part->inode_bitmap.bits, sb_buf->inode_bitmap_sects);
+
+    list_init(&cur_part->open_inodes);
+    printk("mount %s done!\n", part->name);
+
+      return true;
+
+    }
+   return false;     // 使list_traversal继续遍历
+}
+
+
+static void partition_format(struct partition* part) {
+
+    uint32_t boot_sector_sects = 1;
+    uint32_t super_block_sects = 1;
+    uint32_t inode_bitmap_sects = DIV_ROUND_UP(MAX_FILES_PER_PART, BITS_PER_SECTOR);	   // I结点位图占用的扇区数.最多支持4096个文件
+    uint32_t inode_table_sects = DIV_ROUND_UP(((sizeof(struct inode) * MAX_FILES_PER_PART)), SECTOR_SIZE);
+    uint32_t used_sects = boot_sector_sects + super_block_sects + inode_bitmap_sects + inode_table_sects;
+    uint32_t free_sects = part->sec_cnt - used_sects;
+
+    /************** 简单处理块位图占据的扇区数 ***************/
+    uint32_t block_bitmap_sects;
+    block_bitmap_sects = DIV_ROUND_UP(free_sects, BITS_PER_SECTOR);
+    uint32_t block_bitmap_bit_len = free_sects - block_bitmap_sects;
+    block_bitmap_sects = DIV_ROUND_UP(block_bitmap_bit_len, BITS_PER_SECTOR);
+
+     /* 超级块初始化 */
+     struct super_block sb;
+     sb.magic = 0x19590318;
+     sb.sec_cnt = part->sec_cnt;
+     sb.inode_cnt = MAX_FILES_PER_PART;
+     sb.part_lba_base = part->start_lba;
+
+     sb.block_bitmap_lba = sb.part_lba_base + 2;
+     sb.block_bitmap_sects = block_bitmap_sects;
+
+     sb.inode_bitmap_lba = sb.block_bitmap_lba + sb.block_bitmap_sects;
+     sb.inode_bitmap_sects = inode_bitmap_sects;
+
+     sb.inode_table_lba = sb.inode_bitmap_lba + sb.inode_bitmap_sects;
+     sb.inode_table_sects = inode_table_sects;
+
+     sb.data_start_lba = sb.inode_table_lba + sb.inode_table_sects;
+     sb.root_inode_no = 0;
+     sb.dir_entry_size = sizeof(struct dir_entry);
+
+     printk("%s info:\n", part->name);
+     printk("   magic:0x%x\n   part_lba_base:0x%x\n   all_sectors:0x%x\n   inode_cnt:0x%x\n   block_bitmap_lba:0x%x\n   block_bitmap_sectors:0x%x\n   inode_bitmap_lba:0x%x\n   inode_bitmap_sectors:0x%x\n   inode_table_lba:0x%x\n   inode_table_sectors:0x%x\n   data_start_lba:0x%x\n", sb.magic, sb.part_lba_base, sb.sec_cnt, sb.inode_cnt, sb.block_bitmap_lba, sb.block_bitmap_sects, sb.inode_bitmap_lba, sb.inode_bitmap_sects, sb.inode_table_lba, sb.inode_table_sects, sb.data_start_lba);
+
+     struct disk* hd = part->my_disk;
+     ide_write(hd, part->start_lba + 1, &sb, 1);
+     printk("   super_block_lba:0x%x\n", part->start_lba + 1);
+
+     uint32_t buf_size = (sb.block_bitmap_sects >= sb.inode_bitmap_sects ? sb.block_bitmap_sects : sb.inode_bitmap_sects);
+     buf_size = (buf_size >= sb.inode_table_sects ?  buf_size :  sb.inode_table_sects )* SECTOR_SIZE;
+     uint8_t* buf = (uint8_t*)sys_malloc(buf_size);
+
+     buf[0] |= 0x01;
+     uint32_t block_bitmap_last_byte = block_bitmap_bit_len / 8;
+     uint8_t  block_bitmap_last_bit  = block_bitmap_bit_len % 8;
+     uint32_t last_size = SECTOR_SIZE - (block_bitmap_last_byte % SECTOR_SIZE);	     // last_size是位图所在最后一个扇区中，不足一扇区的其余部分
+
+     memset(&buf[block_bitmap_last_byte], 0xff, last_size);
+
+     uint8_t bit_idx = 0;
+     while (bit_idx <= block_bitmap_last_bit) {
+       buf[block_bitmap_last_byte] &= ~(1 << bit_idx++);
+     }
+     ide_write(hd, sb.block_bitmap_lba, buf, sb.block_bitmap_sects);
+
+     /***************************************
+      * 3 将inode位图初始化并写入sb.inode_bitmap_lba *
+      ***************************************/
+      /* 先清空缓冲区*/
+      memset(buf, 0, buf_size);
+      buf[0] |= 0x1;
+
+      ide_write(hd, sb.inode_bitmap_lba, buf, sb.inode_bitmap_sects);
+
+      memset(buf, 0, buf_size);
+      struct inode* i = (struct inode*)buf;
+      i->i_size = sb.dir_entry_size * 2;
+      i->i_no = 0;
+      i->i_sectors[0] = sb.data_start_lba;
+      ide_write(hd, sb.inode_table_lba, buf, sb.inode_table_sects);
+
+      memset(buf, 0, buf_size);
+      struct dir_entry* p_de = (struct dir_entry*)buf;
+
+      /* 初始化当前目录"." */
+      memcpy(p_de->filename, ".", 1);
+      p_de->i_no = 0;
+      p_de->f_type = FT_DIRECTORY;
+      p_de++;
+
+      memcpy(p_de->filename, "..", 2);
+      p_de->i_no = 0;   // 根目录的父目录依然是根目录自己
+      p_de->f_type = FT_DIRECTORY;
+
+      ide_write(hd, sb.data_start_lba, buf, 1);
+
+      printk("   root_dir_lba:0x%x\n", sb.data_start_lba);
+      printk("%s format done\n", part->name);
+      sys_free(buf);
+}
+
+void  filesys_init() {
+    uint8_t channel_no = 0, dev_no, part_idx = 0;
+
+    struct super_block* sb_buf = (struct super_block*) sys_malloc(SECTOR_SIZE);
+
+    if (sb_buf == NULL) {
+       PANIC("alloc memory failed!");
+    }
+    printk("searching filesystem......\n");
+    while (channel_no < channel_cnt) {
+      dev_no = 0;
+      while(dev_no < 2) {
+        if (dev_no == 0) {   // 跨过裸盘hd60M.img
+     	    dev_no++;
+     	    continue;
+     	 }
+       struct disk* hd = &channels[channel_no].devices[dev_no];
+       struct partition* part = hd->primes;
+       while (part_idx < 12) {
+        if (part_idx == 4) {
+          part = hd->logic_parts;
+        }
+
+        if (part->sec_cnt != 0) {
+          memset(sb_buf, 0, SECTOR_SIZE);
+          ide_read(hd, part->start_lba + 1, sb_buf, 1);
+          if (sb_buf->magic == 0x19590318) {
+              printk("%s has filesystem\n", part->name);
+          } else {
+            printk("formatting %s`s partition %s......\n", hd->name, part->name);
+            partition_format(part);
+
+          }
+          part_idx++;
+    	    part++;	// 下一分区
+    	 }
+    	 dev_no++;
+
+        }
+       }
+    }
+
+}
+
+
+static char* path_parse(char* pathname, char* name_store) {
+  if (pathname[0] == '/') {
+     while(*(++pathname) == '/');
+  }
+
+    while(*pathname != '/' && *pathname != 0){
+      *name_store ++ = *pathname++;
+    }
+
+    if (pathname[0] == 0) {   // 若路径字符串为空则返回NULL
+       return NULL;
+    }
+    return pathname;
+}
+
+int32_t path_depth_cnt(char* pathname) {
+    ASSERT(pathname != NULL);
+    char *p = pathname;
+    char name[MAX_FILE_NAME_LEN];       // 用于path_parse的参数做路径解析
+    uint32_t depth = 0;
+
+    p = path_parse(p, name);
+    while(name[0]){
+      depth++;
+      memset(name, 0, MAX_FILE_NAME_LEN);
+      if (p) {	     // 如果p不等于NULL,继续分析路径
+	p  = path_parse(p, name);
+      }
+    }
+    return depth;
+}
+
+static int search_file(const char* pathname, struct path_search_record* searched_record) {
+
+  if (!strcmp(pathname, "/") || !strcmp(pathname, "/.") || !strcmp(pathname, "/..")) {
+      searched_record->parent_dir = &root_dir;
+      searched_record->file_type = FT_DIRECTORY;
+      searched_record->searched_path[0] = 0;	   // 搜索路径置空
+      return 0;
+  }
+
+  uint32_t path_len = strlen(pathname);
+  ASSERT(pathname[0] == '/' && path_len > 1 && path_len < MAX_PATH_LEN);
+  char* sub_path = (char*)pathname;
+  struct dir* parent_dir = &root_dir;
+  struct dir_entry dir_e;
+
+  char name[MAX_FILE_NAME_LEN] = {0};
+  searched_record->parent_dir = parent_dir;
+  searched_record->file_type = FT_UNKNOWN;
+  uint32_t parent_inode_no = 0;
+
+  sub_path = path_parse(sub_path, name);
+  while(name[0]) {
+    ASSERT(strlen(searched_record->searched_path) < 512);
+    strcat(searched_record->searched_path, "/");
+    strcat(searched_record->searched_path, name);
+
+    if (search_dir_entry(cur_part, parent_dir, name, &dir_e)) {
+      memset(name, 0, MAX_FILE_NAME_LEN);
+      if (sub_path) {
+   	    sub_path = path_parse(sub_path, name);
+   	 }
+
+     // 上一步保存的结果
+     if(FT_DIRECTORY == dir_e.f_type) {
+       parent_inode_no = parent_dir->inode->i_no;
+ 	    dir_close(parent_dir);
+      parent_dir = dir_open(cur_part, dir_e.i_no);
+      searched_record->parent_dir = parent_dir;
+	    continue;
+
+     } else if (FT_REGULAR == dir_e.f_type) {	 // 若是普通文件
+  	    searched_record->file_type = FT_REGULAR;
+  	    return dir_e.i_no;
+  	 }
+
+    } else {
+      return -1;
+    }
+  }
+
+  dir_close(searched_record->parent_dir);
+  searched_record->parent_dir = dir_open(cur_part, parent_inode_no);
+  searched_record->file_type = FT_DIRECTORY;
+  return dir_e.i_no;
+
+
+}
